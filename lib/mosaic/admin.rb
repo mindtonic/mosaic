c2db6d23f41d23579c0b02bf1dfe717e0bef1905
Jay Sanders <mindtonic@gmail.com>
2010-06-22 22:15:21 -0400

    Refactored Image

--- lib/mosaic/admin.rb
+++ lib/mosaic/admin.rb
@@ -1,28 +1,14 @@
 module Mosaic 
   class Admin
-  	require 'open-uri'
-  	require 'hpricot'
-  	require 'RMagick'
 		include Magick
 		
-  	attr_accessor :master, :source, :maximum_width, :maximum_height, :canvas, :tiles
+  	attr_accessor :master, :source, :canvas
   
     def initialize(master, source)
-      @master = master
-      @source = source
-	    @maximum_width = 1000
-	    @maximum_height = 1000
-	    @tiles = Array.new
+      @master = Mosaic::Image.new(master)
+      @source = Mosaic::Source.new(source)
     end
-    
-    def resize_master!
-    	@canvas = ImageList.new(@master)
-    	@canvas.change_geometry!("#{@maximum_width}x#{@maximum_height}") {|cols, rows, img| img.resize!(cols, rows)}
-    end
-    
-    def pull_tiles
-    	(Hpricot::XML(open(@source))/"enclosure").each {|z| @tiles << Mosaic::Image.new(z.attributes['url'])}
-    end
+
   end
 end
 
--- lib/mosaic/image.rb
+++ lib/mosaic/image.rb
@@ -1,14 +1,23 @@
 module Mosaic 
   class Image
-  	require 'open-uri'
-  	require 'RMagick'
 		include Magick
 		
-  	attr_accessor :location, :object
+		attr_accessor :location, :magick, :maximum_width, :maximum_height
   
   	def initialize(location)
   		@location = location
+  		@maximum_width = 1000
+  		@maximum_height = 1000
   	end
+  	
+  	def magick_factory
+			@magick ||= Magick::ImageList.new(@location)  	
+  	end
+  	
+    def resize!
+    	magick_factory
+    	@magick.change_geometry!("#{@maximum_width}x#{@maximum_height}") {|cols, rows, img| img.resize!(cols, rows)}
+    end
   
  	end
 end
